4f40f061bbc6f2b8134348aac609c02363c269d4
rubenalba <rubenalba@mbp-de-ruben.marianao.local>
2018-05-09 20:34:47 +0200

    pruebass

--- proyectoRubenAlex/src/dao/GestorConexions.java
+++ proyectoRubenAlex/src/dao/GestorConexions.java
@@ -5,7 +5,7 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class GestorConexions {
-	private static String url = "jdbc:mysql://localhost/proyecto";
+	private static String url = "jdbc:mysql://localhost/proyecto"; //hola!
 	private static String user = "root" ;
 	private static String pwd = "";
 	private static Connection connexio = null;
