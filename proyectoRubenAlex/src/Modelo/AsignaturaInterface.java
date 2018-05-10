c3d1202d534ec0ece9b6c2288d318150f9fe7482
rubenalba <rubenalba@mbp-de-ruben.marianao.local>
2018-05-10 15:49:21 +0200

    Interface Asignaturas

---
+++ proyectoRubenAlex/src/Modelo/AsignaturaInterface.java
@@ -0,0 +1,18 @@
+package Modelo;
+
+import java.util.List;
+
+import pojos.Asignatura;
+
+/**
+ * Asignatura's Interface
+ * @author rubenalba
+ *@version 1.0
+ */
+public interface AsignaturaInterface {
+	public void addAsignatura (int idAsignatura, String nombreAsignatura);
+	public void eliminarAsignatura (int idAsignatura);
+	public void modificarAsignatura (Asignatura asignaturaModificada);
+	public List <Asignatura> verAllAsignaturas();
+
+}
