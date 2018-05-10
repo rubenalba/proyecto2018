472f34dbc7d8b5b9049a1c9e90eee101bec2f3a8
cfgs <cfgs@pc-info-host>
2018-05-10 20:25:17 +0200

    impUF

---
+++ proyectoRubenAlex/src/Modelo/ImpUnidadFormativa.java
@@ -0,0 +1,41 @@
+package Modelo;
+
+import java.util.List;
+
+import pojos.Asignatura;
+import pojos.Profesor;
+import pojos.Unidadformativa;
+
+public class ImpUnidadFormativa implements UnidadFormativaInterface{
+
+	@Override
+	public void addUnidadFormativa(String idUnidadFormativa, Asignatura asignatura, Profesor profesor, int horas) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void eliminarUnidadFormativa(String idUnidadFormativa) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void modificarUnidadFormativa(Unidadformativa unidadFormativaModificada) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public Unidadformativa verUnidadformativaByID(String idUnidadFormativa) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Unidadformativa> verAllUnidadFormativas() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
