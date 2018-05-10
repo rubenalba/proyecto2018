32bbf7d738e78e13691241a0f9d1b11c17918525
rubenalba <rubenalba@mbp-de-ruben.marianao.local>
2018-05-10 15:44:01 +0200

    Alumnos Interface

---
+++ proyectoRubenAlex/src/Modelo/AlumnosInterface.java
@@ -0,0 +1,21 @@
+package Modelo;
+
+import java.util.List;
+
+import pojos.Alumnos;
+import pojos.Asistencia;
+import pojos.Aula;
+
+/**
+ * Alumnos' Interface
+ * @author rubenalba
+ * @version 1.0
+ */
+public interface AlumnosInterface {
+	public void addAlumno (String dni, Aula aula, String nombre, String apellidos, String email);
+	public void eliminarAlumno (String dni);
+	public void modificarAlumno (Alumnos alumnoModificado);
+	public List <Asistencia> verAsistencia(String dni);
+	public Asistencia verAsistencias (String dni);
+	
+}
