29c271fdc742777a46435131311110825b753816
rubenalba <rubenalba@mbp-de-ruben.marianao.local>
2018-05-10 16:36:14 +0200

    aula Interface

---
+++ proyectoRubenAlex/src/Modelo/AulaInterface.java
@@ -0,0 +1,18 @@
+package Modelo;
+
+import java.util.List;
+
+import pojos.Aula;
+
+/**
+ * 
+ * @author rubenalba
+ * @version 1.0  
+ */
+public interface AulaInterface {
+	public void addAula (int numAula);
+	public void eliminarAula (int numAula);
+	public void modificarAula (int numAula);
+	public Aula verAulaByID (int numAula);
+	public List <Aula> verAllAulas (int numAula);
+}
