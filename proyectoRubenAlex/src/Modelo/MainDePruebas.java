29b3595d046ac49e14a1c7ea98be386a38ed58c5
rubenalba <rubenalba@mbp-de-ruben.home>
2018-05-14 12:57:40 +0200

    PRUEBAS DE ADD A LA BD

--- proyectoRubenAlex/src/Modelo/MainDePruebas.java
+++ proyectoRubenAlex/src/Modelo/MainDePruebas.java
@@ -11,12 +11,28 @@ public class MainDePruebas {
 	SessionFactory factory = SessionFactoryUtil.getSessionFactory();
 	static AlumnosInterface alumno = DAO.getAlumnosInterface();
 	static AulaInterface aulas = DAO.getAulaInterface();
-	
-	public static void main(String[] args) {
-		Aula aula = new Aula (1);
-		aulas.addAula(1);
-		/*Alumnos a1 = new Alumnos ("47665701H", aula, "Ruben", "Alba Revilla", "rubenalbarevilla@gmail.com");
-		alumno.addAlumno(a1);*/
-	}
 
+	public static void main(String[] args) {
+		crearDatosdepruebaAulaYAlumno(); //PRUEBAS PARA AULA Y ALUMNO, OK + ó - FUNCIONAR FUNCIONA
+	}
+	public static void crearDatosdepruebaAulaYAlumno() {
+		//EL AULA SE CREA CORRECTAMENTE Y EL ALUMNO EN LA BD, PERO SALE MENSAJE DE EN EL TERMINAL
+		Aula aula = new Aula (2);
+		try {
+			aulas.addAula(aula);
+			System.out.println("Se ha creado el aula");
+		} catch (Exception e) {
+			System.out.println("No se ha creado nada");
+		}
+		try {
+			Alumnos a1 = new Alumnos ("47665701H", aula, "Ruben", "Alba Revilla", "rubenalbarevilla@gmail.com");
+			alumno.addAlumno(a1);
+			System.out.println("alumno creado");
+		} catch (Exception e) {
+			System.out.println("No se ha creado nada");
+		}
+	}
 }
+
+
+
